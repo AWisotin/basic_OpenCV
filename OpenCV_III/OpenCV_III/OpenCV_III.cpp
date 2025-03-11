137cdf9669297fb23e0148e66f845220f65a8b02
AWisotin <alexej.wisotin+github@gmail.com>
2025-03-11 16:43:03 +0100

    ok

---
+++ OpenCV_III/OpenCV_III/OpenCV_III.cpp
@@ -0,0 +1,21 @@
+#include <opencv2/core.hpp>
+#include <opencv2/imgcodecs.hpp>
+#include <opencv2/highgui.hpp>
+#include <iostream>
+using namespace cv;
+using namespace std;
+
+int main()
+{
+    cout << "Hello World!" << endl;
+
+    //cv::Mat img = cv::imread("C:/basic_OpenCV/REF/SS.jpg");
+    //namedWindow("First OpenCV Application", WINDOW_AUTOSIZE);
+    //cv::imshow("First OpenCV Application", img);
+    //cv::moveWindow("First OpenCV Application", 0, 45);
+    //cv::waitKey(0);
+    //cv::destroyAllWindows();
+
+    cout << "OpenCV is OK" << endl;
+    return 0;
+}
